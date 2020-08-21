d7b14411eac6decd1aa6881cdcb91b45a6868250
Carlos R. Arias <arias@spu.edu>
2020-08-21 16:11:11 -0700

    corrected assignment

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http = require('http');
 const hostname = '';
 const port = 5000;
 
-const server == http.createServer((request, response) => {
+const server = http.createServer((request, response) => {
 	response.statusCode = 200;
 	response.setHeader('Content-Type', 'text/plain');
 	response.end('Hello World\n\nCool!');
